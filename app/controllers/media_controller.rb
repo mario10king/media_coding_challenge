b59c228d93bdfc938b205b1241ccc93dd43a9429
Mario Marroquin <mariomarroquin@marios-mbp.attlocal.net>
2018-09-13 09:55:18 -0700

    Make external API call to implement search

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,3 +1,6 @@
+require 'uri'
+require 'net/http'
+
 class MediaController < ApplicationController
   def shows
     id = params[:id]
@@ -11,6 +14,18 @@ class MediaController < ApplicationController
 
   def search
     query = params[:query]
-    puts query 
+    api_key = ENV['TMDB_API_KEY']
+
+    url = URI("https://api.themoviedb.org/3/search/multi?query=#{query}&api_key=#{api_key}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request.body = "{}"
+
+    response = http.request(request)
+    puts response.read_body
   end
 end

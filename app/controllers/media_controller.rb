e26e5803087ddbaec41c8587a94e331cb8ecc2ef
Mario Marroquin <mariomarroquin@marios-mbp.attlocal.net>
2018-09-13 17:07:55 -0700

    Exctract relevant info from API response and render it in JSON.

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -5,19 +5,25 @@ class MediaController < ApplicationController
   def show
     id = params[:id]
     query_string  = "/tv/#{id}?"
-    puts call_api(query_string)
+    response = call_api(query_string)
+
+    render json: format(response)
   end
 
   def movie 
     id = params[:id]
     query_string  = "/movie/#{id}?"
-    puts call_api(query_string)
+    response = call_api(query_string)
+ 
+    render json: format(response)
   end
 
   def search
     query = params[:query]
     query_string  = "/search/multi?query=#{query}"
-    puts call_api(query_string)
+    response = call_api(query_string)
+
+    render json: format_search(response) 
   end
 
   def call_api(query_string)
@@ -33,5 +39,18 @@ class MediaController < ApplicationController
 
     response = http.request(request)
     response.read_body
+  end
+
+  def format(response)
+    media_info = JSON.parse(response)
+    release = media_info["first_air_date"] || media_info["release_date"]
+    title = media_info["name"] || media_info["title"]
+    synopsis = media_info["overview"]
+
+    {title: title, release: release, synopsis: synopsis}.to_json
+  end
+  
+  def format_search(response)
+
   end
 end
